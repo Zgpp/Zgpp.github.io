f61b6309fd242fb40496b6721ff85c22dcdf4320
Zgpp <121903909@qq.com>
2019-04-03 11:16:54 -0400

    class

--- hw/jquery1/js/script.js
+++ hw/jquery1/js/script.js
@@ -1,5 +1,6 @@
 //Executes when page loads and it's ready to view
 var step = 0;
+
 var titles = [
   'Prepare for take-off', //0 title[0]
   'Put seatbelt on', //1 title[1]
@@ -10,24 +11,71 @@ var titles = [
 
 $(function(){
  $('#stepinfo, section, footer, .prev').hide();
-
 });
 
+//We use the start button to begin our tutorial
 $('.start').on('click', function(){
   //do stuff when the button is clicked
   $('.intro').hide();
   $('footer, #stepinfo').show();
   step = 1;
+
   $('#step'+step).show();
   //set the title for our current step
-  $('#stepinfo').text('Step'+step+': 'titles[step-1]);
+  $('#stepinfo').text('Step'+step+': '+titles[step-1]);
 });
 
+//Next to advance each slide
 $('.next').on('click', function(){
-  //step = step + 1;
   $('#step'+step).hide();
+  //step = step + 1;
   step++;
-  $('#step'+step).show();
-  $('#stepinfo').text('Step'+step+': 'titles[step-1]);
+  if(step == 2) {
+    $('.prev').show();
+  }
+
+  if(step == 5) {
+    $('.next').hide();
+  }
+
+  //make the change only if we are under our number of 6
+  if (step <= 5){
+    $('#step'+step).show();
+    $('#stepinfo').text('Step'+step+': '+titles[step-1]);
+  }
+  else {
+      step--;
+      $('#step'+step).show();
+      $('.next').hide();
+    }
+
+
+});
+
+//Previous
+$('.prev').on('click', function(){
+  $('#step'+step).hide();
+  //step = step + 1;
+  step--;
+  if(step == 1) {
+    $('.prev').hide();
+  } else {
+    $('.next').show();
+  }
+
+  if(step == 5) {
+    $('.next').hide();
+  }
+
+  //make the change only if we are under our number of 6
+  if (step >= 1){
+    $('#step'+step).show();
+    $('#stepinfo').text('Step'+step+': '+titles[step-1]);
+  }
+  else {
+      step--;
+      $('#step'+step).show();
+    }
+
 
 });
